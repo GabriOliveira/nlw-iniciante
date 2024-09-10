20f0616ab0207bf4c50dc0024b7c93365df9cbd8
GabriOliveira <gabriel007og@gmail.com>
2024-09-09 22:39:28 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-//Hello World
+//arrays, objetos
+let meta = {
+    value: 'estudar todo dia',//value do meta;
+    checked: false,
+    log: (info) => {//método
+        console.log(info)
+    }
+}
+meta.value = 'Não é mais estudar todo dia'
+meta.log(meta.value);
+//pega o objeto(meta) busca a propriedade chamada(log) no metodo é passado o argumento 
+//info, na (meta.log()) é inserido no argumento(info) valor do argumento => (meta.value), isso ira retornar o value de meta;
 
-console.log(op);
+//function
+const criarMeta = () => {
+
+}
